92dbb4edc2d8f3158b1f885095695ab1c91b8917
Mariia2811 <mary.stupka11@gmail.com>
2023-06-15 17:37:18 +0300

    comit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,60 @@
-export const App = () => {
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+
+export function App() {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
+
+  const handleAddContact = (newContact) => {
+    const isDuplicate = contacts.some(
+      (contact) => contact.name && contact.name.toLowerCase() === newContact.name.toLowerCase()
+    );
+
+    if (isDuplicate) {
+      alert(`Contact "${newContact.name}" already exists.`);
+    } else {
+      setContacts((prevContacts) => [...prevContacts, newContact]);
+    }
+  };
+
+  const handleDeleteContact = (contactId) => {
+    const updatedContacts = contacts.filter((contact) => contact.id !== contactId);
+    setContacts(updatedContacts);
+  };
+
+  const handleFilterChange = (filterValue) => {
+  setFilter(filterValue);
+};
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name && contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phone Book</h1>
+      <Filter value={filter} onChange={handleFilterChange} />
+      <ContactForm onAddContact={handleAddContact} />
+      <h2>Contact List</h2>
+      <ContactList contacts={filteredContacts} onDeleteContact={handleDeleteContact} />
     </div>
   );
+}
+
+App.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
 };
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
+import PropTypes from 'prop-types';
+
+export function ContactForm({ onAddContact }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNumber(event.target.value);
+  };
+
+  const handleAddContact = () => {
+    if (name.trim() !== '' && number.trim() !== '') {
+      const newContact = {
+        id: nanoid(),
+        name: name.trim(),
+        number: number.trim(),
+      };
+
+      onAddContact(newContact);
+      setName('');
+      setNumber('');
+    }
+  };
+
+  return (
+    <form>
+      <input
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+        value={name}
+        onChange={handleNameChange}
+      />
+
+      <input
+        type="tel"
+        name="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+        value={number}
+        onChange={handleNumberChange}
+      />
+
+      <button type="button" onClick={handleAddContact}>
+        Add Contact
+      </button>
+    </form>
+  );
+}
+
+ContactForm.propTypes = {
+  onAddContact: PropTypes.func.isRequired,
+};
